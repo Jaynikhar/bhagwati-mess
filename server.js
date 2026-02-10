2c2fb11dd7a602b4ff5c8e898e72766feb312576
Jaynikhar <jaynikhar61@gmail.com>
2026-02-11 04:38:37 +0530

    edit

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://your-frontend.vercel.app"
-  ],
-  credentials: true
+  ]
 }));
 
 app.use(express.json());
--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://your-frontend.vercel.app"
-  ],
-  credentials: true
+  ]
+
 }));
 app.use(express.json());
 
